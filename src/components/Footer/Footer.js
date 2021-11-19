904759d0a3366ba03b2de099778252872fdfd580
DAN <DANKIBIWOTT254@GMAIL.COM>
2021-11-19 15:56:04 +0300

    push update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const FooterSection = styled.div`
-  background-image: url(https://raw.githubusercontent.com/gurupawar/portfolio/main/assets/footer_wave.svg);
+  background-image: url();
   background-repeat: no-repeat;
   background-size: cover;
   width: 100%;
