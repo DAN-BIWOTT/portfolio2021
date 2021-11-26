07e3eb36912a93920170ea6ae0f68f459824dd3a
DAN <DANKIBIWOTT254@GMAIL.COM>
2021-11-26 09:12:17 +0300

    resume

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -89,7 +89,7 @@ function Dropdown({ isOpen, toggle }) {
       <NavBtn onClick={toggle}>
         <a
           className="btn PrimaryBtn"
-          href="https://github.com/DAN-BIWOTT/DAN-BIWOTT.github.io/blob/master/docs/resume.docx?raw=true"
+          href="https://docs.google.com/document/d/15x7QBuBbtmEeqRxD_rBneFYxy3d8w751/edit?usp=sharing&ouid=103112845827557686303&rtpof=true&sd=true"
           target="_blank"
           rel="noopener noreferrer"
         >
